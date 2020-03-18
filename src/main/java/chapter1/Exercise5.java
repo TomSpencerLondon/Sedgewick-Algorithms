f8c2c761c2e0c954deaaa6c7be8f6727490784b1
Tom Spencer <tom@homeflow.co.uk>
2020-03-18 07:58:04 +0000

    Add recursive binaryConverter function. This uses guard of if (num > 0) then assigns num % 2 to int y and then returns itself with paraater s num / 2 + "" + y

--- src/main/java/chapter1/Exercise5.java
+++ src/main/java/chapter1/Exercise5.java
@@ -3,10 +3,11 @@ package chapter1;
 public class Exercise5 {
 
   public static void main(String[] args){
-    System.out.println(exerciseFive(5));
+//    System.out.println(exerciseFive(5));
+    System.out.println(binaryConverter(5));
   }
 
-  // convert number to binary with for loop
+  // 1. convert number to binary with for loop
   public static String exerciseFive(int num){
     String binary = "";
     for (int n = num; n > 0; n /= 2) {
@@ -14,6 +15,16 @@ public class Exercise5 {
     }
 
     return binary;
+  }
+
+  // 2. convert number to binary with recursion
+  public static String binaryConverter(int num){
+    int y;
+    if (num > 0){
+      y = num % 2;
+      return (binaryConverter(num / 2) + "" + y);
+    }
+    return "";
   }
 }
 
